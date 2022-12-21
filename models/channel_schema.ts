e4e02972b8447d7d9d3fc5ca997d7609f32f23af
shdewz <shdewz@gmail.com>
2022-12-21 19:18:37 +0200

    Include gamemode in channel schema

--- models/channel_schema.ts
+++ models/channel_schema.ts
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const channelSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
-    last_beatmap: Number
+    last_beatmap: {
+        id: Number,
+        mode: String
+    }
 });
 
 const model = mongoose.model('channels', channelSchema);
