b4e79f59f0274478805da682355a70b0aebee55b
shdewz <shdewz@gmail.com>
2023-05-20 17:52:27 +0300

    Add option to clear guild commands

--- handlers/command_handler.ts
+++ handlers/command_handler.ts
@@ -17,7 +17,21 @@ module.exports = (client) => {
     const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
     (async () => {
         try {
-            if (process.env.ENV === 'production') await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+            if (process.env.ENV === 'production') {
+                await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+
+                if (process.env.CLEAR_COMMANDS === 'TRUE') {
+                    await rest.get(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEV_GUILD))
+                        .then(data => {
+                            const promises = [];
+                            for (const command of data) {
+                                const deleteUrl = `${Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEV_GUILD)}/${command.id}`;
+                                promises.push(rest.delete(deleteUrl));
+                            }
+                            return Promise.all(promises);
+                        });
+                }
+            }
             else await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEV_GUILD), { body: commands });
 
             console.log('Successfully reloaded application commands.');
