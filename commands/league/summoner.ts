162a4ca0ab36048705f1916264928fb06e7f4685
dain98 <dain98@gmail.com>
2023-06-01 21:17:44 +0000

    init commit

---
+++ commands/league/summoner.ts
@@ -0,0 +1,6 @@
+import { ChatInputCommandInteraction } from "discord.js";
+import userModel from '../../models/user_schema.js';
+
+module.exports = async (interaction: ChatInputCommandInteraction) => {
+    
+}
